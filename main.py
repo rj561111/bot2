fea91ec41ceed643d2af848ad2e61976f0fb9e1f
rj561111 <rjovan567@gmail.com>
2025-11-16 15:10:20 -0500

    Update

--- main.py
+++ main.py
@@ -3,23 +3,14 @@ from discord.ext import commands
 import os, random
 import requests
 
-# --------------------------------------------------
-# CONFIGURACIÓN DEL BOT
-# --------------------------------------------------
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-# --------------------------------------------------
-# EVENTOS
-# --------------------------------------------------
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-# --------------------------------------------------
-# COMANDOS BÁSICOS
-# --------------------------------------------------
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hola, soy {bot.user}!')
@@ -35,16 +26,11 @@ async def duck(ctx):
     image_url = get_duck_image_url()
     await ctx.send(image_url)
 
-# --------------------------------------------------
-# SISTEMA DE MEMES CON CATEGORÍA Y RARIDAD
-# --------------------------------------------------
 @bot.command()
 async def meme(ctx, categoria=None, raridad=None):
 
-    # Categorías permitidas
     categorias_validas = ["animales", "programacion", "random"]
 
-    # Raridades y probabilidades por defecto
     raridades = {
         "comun": 0.60,
         "raro": 0.25,
@@ -52,15 +38,12 @@ async def meme(ctx, categoria=None, raridad=None):
         "legendario": 0.05
     }
 
-    # Si no dan categoría → elegir al azar
     if categoria is None:
         categoria = random.choice(categorias_validas)
 
-    # Validar categoría
     if categoria not in categorias_validas:
         return await ctx.send(f"Categoría inválida.\nUsa: {', '.join(categorias_validas)}")
 
-    # Si no dan raridad → se elige según probabilidades
     if raridad is None:
         raridad = random.choices(
             population=list(raridades.keys()),
@@ -68,20 +51,16 @@ async def meme(ctx, categoria=None, raridad=None):
             k=1
         )[0]
 
-    # Validar raridad
     if raridad not in raridades.keys():
         return await ctx.send("Raridad inválida.\nUsa: comun, raro, epico o legendario")
 
-    # Ruta final
     carpeta = f"Imagenes/{categoria}/{raridad}"
 
-    # Verificar imágenes en carpeta
     try:
         imagen = random.choice(os.listdir(carpeta))
     except:
         return await ctx.send(f"No hay imágenes en {categoria}/{raridad}")
 
-    # Enviar imagen
     with open(f"{carpeta}/{imagen}", "rb") as img:
         await ctx.send(
             f"Meme de {categoria} | Raridad: {raridad.upper()}",
